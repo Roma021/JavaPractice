c9a76def7ec38c83d38548940766e036d950f89d
Roma021 <dtmogh@gmail.com>
2019-06-17 16:01:21 -0400

    PracticeJava

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+
+public class Main {
+    public static void main(String [] args) {
+        //System.out.print("Hello world!");
+        //System.out.print("Hello\tworld!\nand\tall\tthe\tpeople\tof\tthe\tworld.");
+        //System.out.println("First");
+        //System.out.println("Second");
+        //System.out.println("Third");
+        //System.out.println("Text to be printed");
+        //System.out.println("Davoud\tTaslim\tMoghadam");
+
+        //System.out.println("Hello world!\n(And all the people of the world) ");
+       // System.out.println("(And all the people of the world)");
+
+        //System.out.println("    *");
+        //System.out.println("   ***");
+        //System.out.println("  *****");
+        //System.out.println(" *******");
+        //System.out.println("*********");
+        //System.out.println("    *");
+
+        //int numbersList = 24;
+        //System.out.println("The number is: " + numbersList);
+        //numbersList = 32;
+        //System.out.println("The new number is?\t" + numbersList);
+        //String wordList = "Hello how are you!";
+        //System.out.println("Hi, " + wordList);
+        //double decimal = 3.213;
+        //System.out.println("The decimal number is:\t" + decimal);
+        //boolean x = true;
+        //System.out.println("Is the statement true?\t" + x );
+
+       //nt chickens = 9000;
+       //ouble baconKg =0.1;
+       //tring tractor = "Zetor";
+
+    //  System.out.println("chickens: ");
+    //  System.out.println(chickens);
+    //  System.out.println("Bacon (Kg):");
+    //  System.out.println(baconKg);
+    //  System.out.println("A tractor:");
+    //  System.out.println(tractor);
+    //  System.out.println("\n");
+    //  System.out.println("In a nutshell:");
+    //  System.out.println(chickens);
+    //  System.out.println(baconKg);
+    //  System.out.println(tractor);
+}
+}
---
+++ src/New.java
@@ -0,0 +1,42 @@
+public class New {
+    public static void main(String [] args) {
+       // int x = 21;
+       // int y = 42;
+       // int sum = x +y;
+       // System.out.println("The sum is: " + sum);
+       // double sum = (double)3 / 2 ;
+       // System.out.println("The sum is: " + sum);
+
+        //double decimal = 3.0 / 2 ;
+       // System.out.println("The sum is: " + decimal);
+
+       // int dividend = 3;
+       // int divisor = 2;
+
+       // double remainder = 3/ 2* 1.0;
+       // System.out.println("The remainder is: " + remainder);
+
+        //int secondsInYear = 365 * 24 * 60 *60;
+        //System.out.println("There are " + secondsInYear + " seconds in a year.");
+
+        //int daysInYear = 365;
+      //  int hoursInDay = 24;
+      //  int minutesInHour = 60;
+      //  int secondsInMinute = 60;
+      //  int secondsInYear = daysInYear * hoursInDay * minutesInHour * secondsInMinute;
+      //  System.out.println("There are " + secondsInYear + " seconds in a year.");
+
+        int x = 73457;
+        int y = 12888;
+        int sum = x+y;
+        System.out.println(x + " + " + y + " = " + sum);
+
+        int a = 2;
+        int b = 8;
+        int sumB = a * b;
+        System.out.println(a + " * " + b + " = " + sumB);
+
+
+
+    }
+}
